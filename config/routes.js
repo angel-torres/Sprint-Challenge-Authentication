6e34609f15c2d39933ad0511d13662cb9c861af2
Angel Torres <alas4224@gmail.com>
2019-03-01 11:50:28 -0600

    finished MVP

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
 const db = require('../database/dbConfig');
-const secret = require('../auth/authenticate');
+const secret = require('../auth/authenticate').jwtKey;
 const jwt = require('jsonwebtoken');
 
 const { authenticate } = require('../auth/authenticate');
@@ -21,7 +21,7 @@ function generateToken(user) {
     expiresIn: '1hr'
   }
 
-  return jwt.sign(payload, secret.jwtKey, options)
+  return jwt.sign(payload, secret, options)
 }
 
 function register(req, res) {
@@ -41,13 +41,28 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
+  const { username, password } = req.body
+  if (username && password) {
+    db('users').where({username}).first()
+    .then(response => {
+      const user = response
+      if (user) {
+        const token = generateToken(response.username)
+        res.status(200).json({token: token})
+      } else {
+        res.status(400).json({message:'user not found'})
+      }
+    })
+    .catch(err => req.status(500).json({message:'there was a problem.'}))
+  } else {
+    res.status(400).json({message: 'please provede username and password to login.'})
+  }
 }
 
 function getJokes(req, res) {
   const requestOptions = {
     headers: { accept: 'application/json' },
   };
-
   axios
     .get('https://icanhazdadjoke.com/search', requestOptions)
     .then(response => {
