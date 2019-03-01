89398f8c7d595e144da6be458adc3a353d9fc07b
Angel Torres <alas4224@gmail.com>
2019-03-01 11:10:08 -0600

    add register functionality

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig');
+const secret = require('../auth/authenticate');
+const jwt = require('jsonwebtoken');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -8,8 +12,31 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+function generateToken(user) {
+  const payload = {
+    subject: user
+  }
+
+  const options = {
+    expiresIn: '1hr'
+  }
+
+  return jwt.sign(payload, secret.jwtKey, options)
+}
+
 function register(req, res) {
   // implement user registration
+  const user = req.body
+  const token = generateToken(user.username);
+  if (user.username && user.password) {
+    user.password = bcrypt.hashSync(user.password, 12);
+    db('users').insert(user)
+    .then(response => {
+      res.status(200).json({user: user.username, token})
+    })
+  } else {
+    res.status(400).json({message: 'please provede username and password to register.'})
+  }
 }
 
 function login(req, res) {
